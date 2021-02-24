d56e0325065c3dfbd780a9221f48d70ca7516702
rking <robinking89m@gmail.com>
2021-02-24 18:52:43 +0100

    turning abstraction-layer into a library

--- domain/src/main/java/de/dhbw/foodcoop/warehouse/domain/Good.java
+++ domain/src/main/java/de/dhbw/foodcoop/warehouse/domain/Good.java
@@ -1,10 +1,12 @@
 package de.dhbw.foodcoop.warehouse.domain;
+import org.apache.commons.lang3.Validate;
 
 public class Good {
     private final String name;
     private final Kind kind;
 
     public Good(String name, Kind kind) {
+        Validate.notBlank(name);
         this.name = name;
         this.kind = kind;
     }
