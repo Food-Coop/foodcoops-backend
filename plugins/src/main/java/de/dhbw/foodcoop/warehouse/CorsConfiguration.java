5c2af4515f838881f2014f0e502195fed4eeffee
Stefan Lukas <etaloof@gmail.com>
2021-04-03 01:31:20 +0200

    Add permission and insanely insecure CORS configuration to make things work

---
+++ plugins/src/main/java/de/dhbw/foodcoop/warehouse/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package de.dhbw.foodcoop.warehouse;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+            }
+        };
+    }
+
+}
