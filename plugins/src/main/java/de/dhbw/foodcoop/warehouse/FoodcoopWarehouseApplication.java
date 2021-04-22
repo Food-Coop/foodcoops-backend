887baea47b3959b363faeddc7cb0a503aae2b28d
rking <robinking89m@gmail.com>
2021-04-22 16:40:02 +0200

    no more dummy data

--- plugins/src/main/java/de/dhbw/foodcoop/warehouse/FoodcoopWarehouseApplication.java
+++ plugins/src/main/java/de/dhbw/foodcoop/warehouse/FoodcoopWarehouseApplication.java
@@ -1,17 +1,7 @@
 package de.dhbw.foodcoop.warehouse;
 
-import de.dhbw.foodcoop.warehouse.domain.entities.Kategorie;
-import de.dhbw.foodcoop.warehouse.domain.entities.Produkt;
-import de.dhbw.foodcoop.warehouse.domain.repositories.KategorieRepository;
-import de.dhbw.foodcoop.warehouse.domain.utils.TestUtils;
-import de.dhbw.foodcoop.warehouse.domain.values.Einheit;
-import de.dhbw.foodcoop.warehouse.domain.values.Lagerbestand;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class FoodcoopWarehouseApplication {
