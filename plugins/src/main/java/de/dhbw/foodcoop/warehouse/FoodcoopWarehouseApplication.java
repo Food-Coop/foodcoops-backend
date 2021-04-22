d8c556314970e46a8811fee312c869da2d893781
rking <robinking89m@gmail.com>
2021-04-22 16:39:43 +0200

    no more dummy data

--- plugins/src/main/java/de/dhbw/foodcoop/warehouse/FoodcoopWarehouseApplication.java
+++ plugins/src/main/java/de/dhbw/foodcoop/warehouse/FoodcoopWarehouseApplication.java
@@ -19,43 +19,4 @@ public class FoodcoopWarehouseApplication {
     public static void main(String[] args) {
         SpringApplication.run(FoodcoopWarehouseApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner demoGemuese(KategorieRepository repository) {
-        return (args) -> repository.speichern(initKategorieGemuese());
-    }
-
-    @Bean
-    public CommandLineRunner demoTeigwaren(KategorieRepository repository) {
-        return (args) -> repository.speichern(initKategorieTeigwaren());
-    }
-
-    private Kategorie initKategorieGemuese() {
-        Einheit einheit = new Einheit(TestUtils.EINHEIT_TEST_ID, "kg");
-        Lagerbestand lagerbestand1 = new Lagerbestand(einheit, 12.2, 15.0);
-        Lagerbestand lagerbestand2 = new Lagerbestand(einheit, 13.1, 18.0);
-        Produkt karrotten = new Produkt(TestUtils.PRODUKT_TEST_ID, "Karrotten", null, lagerbestand1);
-        Produkt roteBeete = new Produkt("Rote Beete", null, lagerbestand2);
-        Kategorie gemuese = new Kategorie(TestUtils.KATEGORIE_TEST_ID
-                ,"Gemüse"
-                , TestUtils.GEMUESEICON
-                , Arrays.asList(karrotten, roteBeete));
-        karrotten.setKategorie(gemuese);
-        roteBeete.setKategorie(gemuese);
-        return gemuese;
-    }
-
-    private Kategorie initKategorieTeigwaren() {
-        Einheit einheit = new Einheit(TestUtils.EINHEIT_TEST_ID ,"kg");
-        Lagerbestand lagerbestand1 = new Lagerbestand(einheit, 12.1, 15.9);
-        Lagerbestand lagerbestand2 = new Lagerbestand(einheit, 13.0, 18.1);
-        Produkt weissbrot = new Produkt("Kasten Weißbrot", null, lagerbestand1);
-        Produkt roggenbrot = new Produkt("Roggenbrot", null, lagerbestand2);
-        Kategorie teigwaren = new Kategorie("Teigwaren"
-                , TestUtils.TEIGWARENICON
-                , Arrays.asList(weissbrot, roggenbrot));
-        weissbrot.setKategorie(teigwaren);
-        roggenbrot.setKategorie(teigwaren);
-        return teigwaren;
-    }
 }
