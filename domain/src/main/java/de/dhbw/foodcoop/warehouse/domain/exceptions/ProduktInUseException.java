43ba59734902296cf327ac91a2c23d47b53eaec1
rking <robinking89m@gmail.com>
2021-04-24 15:07:55 +0200

    adding produkt in use handler

--- domain/src/main/java/de/dhbw/foodcoop/warehouse/domain/exceptions/ProduktInUseException.java
+++ domain/src/main/java/de/dhbw/foodcoop/warehouse/domain/exceptions/ProduktInUseException.java
@@ -2,6 +2,6 @@ package de.dhbw.foodcoop.warehouse.domain.exceptions;
 
 public class ProduktInUseException extends RuntimeException {
     public ProduktInUseException(String id) {
-        super("The Produkt " + id + " is still in stock in.");
+        super("The Produkt " + id + " is still in stock.");
     }
 }
