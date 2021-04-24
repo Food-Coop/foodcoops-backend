d29a0c7df071bf30d07868830f5d435a69d28f3b
rking <robinking89m@gmail.com>
2021-04-24 14:33:48 +0200

    added handling not found einheit

--- domain/src/main/java/de/dhbw/foodcoop/warehouse/domain/exceptions/EinheitNotFoundException.java
+++ domain/src/main/java/de/dhbw/foodcoop/warehouse/domain/exceptions/EinheitNotFoundException.java
@@ -2,6 +2,6 @@ package de.dhbw.foodcoop.warehouse.domain.exceptions;
 
 public class EinheitNotFoundException extends RuntimeException {
     public EinheitNotFoundException(String id) {
-        super("Could not find Produkt " + id);
+        super("Could not find Einheit " + id);
     }
 }
---
+++ plugins/src/main/java/de/dhbw/foodcoop/warehouse/plugins/rest/advice/EinheitNotFoundAdvice.java
@@ -0,0 +1,18 @@
+package de.dhbw.foodcoop.warehouse.plugins.rest.advice;
+
+import de.dhbw.foodcoop.warehouse.domain.exceptions.EinheitNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class EinheitNotFoundAdvice {
+    @ResponseBody
+    @ExceptionHandler(EinheitNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String einheitNotFoundHandler(EinheitNotFoundException exception) {
+        return exception.getMessage();
+    }
+}
