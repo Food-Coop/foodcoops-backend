433225f4ae75404e8b53452226af8c8c03433ec1
Matteo Staar <matteo.staar@cjt.de>
2024-03-13 18:12:48 +0100

    test

--- plugins/src/main/java/de/dhbw/foodcoop/warehouse/plugins/rest/EinkaufController.java
+++ plugins/src/main/java/de/dhbw/foodcoop/warehouse/plugins/rest/EinkaufController.java
@@ -1,5 +1,6 @@
 package de.dhbw.foodcoop.warehouse.plugins.rest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.dhbw.foodcoop.warehouse.application.einkauf.EinkaufService;
+import de.dhbw.foodcoop.warehouse.application.frischbestellung.FrischBestellungService;
 import de.dhbw.foodcoop.warehouse.domain.entities.BestandBuyEntity;
 import de.dhbw.foodcoop.warehouse.domain.entities.BestandEntity;
 import de.dhbw.foodcoop.warehouse.domain.entities.BestellungEntity;
 import de.dhbw.foodcoop.warehouse.domain.entities.EinkaufEntity;
+import de.dhbw.foodcoop.warehouse.domain.entities.FrischBestellung;
 import de.dhbw.foodcoop.warehouse.domain.values.EinkaufBestellungVergleich;
 
 @RestController
@@ -21,13 +24,41 @@ public class EinkaufController {
 	
     private final EinkaufService einkaufService;
     
+    
+    @Autowired
+    private FrischBestellungService fbs;
+    
     @Autowired
     public EinkaufController(EinkaufService einkaufService) {
     	this.einkaufService = einkaufService;
     }
     
+    @GetMapping(value = "/example")
+    public EinkaufBestellungVergleich testEBV() {
+    	FrischBestellung fb = fbs.findById("da29d20a-86c3-4fea-a24f-a5b1e7d01cb9").get();
+    	return einkaufService.createCompareObjectForPersonOrder(fb, 2);
+    }
+    
+    @GetMapping(value = "/example/asList")
+    public List<EinkaufBestellungVergleich> testEBV2() {
+    	FrischBestellung fb = fbs.findById("da29d20a-86c3-4fea-a24f-a5b1e7d01cb9").get();
+    	
+    	List<EinkaufBestellungVergleich> test = new ArrayList<>();
+    	test.add(einkaufService.createCompareObjectForPersonOrder(fb, 2));
+    	return test;
+    }
+    
+    
+    @GetMapping(value = "/example/empty")
+    public List<BestandBuyEntity> testEBV3() {
+    	
+    	List<BestandBuyEntity> test = new ArrayList<>();
+    	return test;
+    }
+    
     @GetMapping(value = "/einkaufe")
     public List<EinkaufEntity> einkäufe() {
+    	
     	return einkaufService.all();
     }
     
