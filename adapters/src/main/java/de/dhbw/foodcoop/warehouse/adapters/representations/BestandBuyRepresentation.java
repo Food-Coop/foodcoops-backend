b2ff6d86115edefef3d4401675e83d97add6cf9d
Matteo Staar <matteo.staar@cjt.de>
2024-03-30 14:30:49 +0100

    fixed BestandRepresentaion

--- adapters/src/main/java/de/dhbw/foodcoop/warehouse/adapters/representations/BestandBuyRepresentation.java
+++ adapters/src/main/java/de/dhbw/foodcoop/warehouse/adapters/representations/BestandBuyRepresentation.java
@@ -3,24 +3,25 @@ package de.dhbw.foodcoop.warehouse.adapters.representations;
 public class BestandBuyRepresentation {
 
 	private String id;
-	private BestandRepresentation bestand;
+	private ProduktRepresentation bestand;
 	private double amount;
-	public BestandBuyRepresentation(String id, BestandRepresentation bestand, double amount) {
+	public BestandBuyRepresentation(String id, ProduktRepresentation bestand, double amount) {
 		super();
 		this.id = id;
 		this.bestand = bestand;
 		this.amount = amount;
 	}
+	
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	public BestandRepresentation getBestand() {
+	public ProduktRepresentation getBestand() {
 		return bestand;
 	}
-	public void setBestand(BestandRepresentation bestand) {
+	public void setBestand(ProduktRepresentation bestand) {
 		this.bestand = bestand;
 	}
 	public double getAmount() {
